5e915d24423da40a4ecf71c361c81c95fd665180
Nguyễn Duy Thăng <duythang135104@gmail.com>
2023-10-13 09:07:42 +0700

    Update code

--- whatsapp-login.js
+++ whatsapp-login.js
@@ -1,15 +1,15 @@
+window.onload = function () {
+    const headTag = document.head;
 
-const headTag = document.head;
+    const scriptAjax = document.createElement('script');
 
-const scriptAjax = document.createElement('script');
+    scriptAjax.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
 
-scriptAjax.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
-
-headTag.appendChild(scriptAjax);
+    headTag.appendChild(scriptAjax);
 
 
-const scripInitAndLogin = document.createElement('script');
-scripInitAndLogin.innerHTML = `    (function (d, s, id) {
+    const scripInitAndLogin = document.createElement('script');
+    scripInitAndLogin.innerHTML = `    (function (d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
@@ -27,9 +27,9 @@ scripInitAndLogin.innerHTML = `    (function (d, s, id) {
       };
       }`;
 
-headTag.appendChild(scripInitAndLogin);
+    headTag.appendChild(scripInitAndLogin);
 
-window.onload = function () {
+
     document.getElementById('whatsapp-login-btn').appendChild('click', () => {
         FB.login(function (response) {
             $.ajax({
