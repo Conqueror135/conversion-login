1865e8f0eb3dd3aba331b87083328de1a72c54a8
Nguyễn Duy Thăng <duythang135104@gmail.com>
2023-10-13 08:55:35 +0700

    Update messenger-login

--- messenger-login.js
+++ messenger-login.js
@@ -1,15 +1,15 @@
-$(document).ready(function() { 
-    const headTag = document.head;
 
-    const scriptAjax = document.createElement('script');
+const headTag = document.head;
 
-    scriptAjax.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
+const scriptAjax = document.createElement('script');
 
-    headTag.appendChild(scriptAjax);
+scriptAjax.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
+
+headTag.appendChild(scriptAjax);
 
 
-    const scripInitAndLogin = document.createElement('script');
-    scripInitAndLogin.innerHTML = `    (function (d, s, id) {
+const scripInitAndLogin = document.createElement('script');
+scripInitAndLogin.innerHTML = `    (function (d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
@@ -27,27 +27,30 @@ $(document).ready(function() {
       };
       }`;
 
-    headTag.appendChild(scripInitAndLogin);
-    document.getElementById('messenger-login-btn').appendChild('click',()=>{
+headTag.appendChild(scripInitAndLogin);
+
+window.onload = function () {
+    document.getElementById('messenger-login-btn').appendChild('click', () => {
         FB.login(function (response) {
             $.ajax({
-              url: '/auth/messenger/callback',
-              method: 'POST',
-              data: response,
-              success: function (data) {
-                console.log('Gửi dữ liệu thành công', data);
-              },
-              error: function (error) {
-                console.error('Lỗi khi gửi dữ liệu', error);
-              }
+                url: '/auth/messenger/callback',
+                method: 'POST',
+                data: response,
+                success: function (data) {
+                    console.log('Gửi dữ liệu thành công', data);
+                },
+                error: function (error) {
+                    console.error('Lỗi khi gửi dữ liệu', error);
+                }
             });
-    
-          }
+
+        }
             ,
             {
-              config_id: '1020964145774694',
-              response_type: 'code',
-              override_default_response_type: true
+                config_id: '1020964145774694',
+                response_type: 'code',
+                override_default_response_type: true
             });
     })
-});
+};
+
